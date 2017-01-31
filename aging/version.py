871ebb2f6554ef82b3facd75ff9b56b570d339e0
erramuzpe <asier.erramuzpe@gmail.com>
2017-01-31 12:38:10 +0100

    updated versio.py, setup.py and travis file

--- aging/version.py
+++ aging/version.py
@@ -21,24 +21,17 @@ CLASSIFIERS = ["Development Status :: 3 - Alpha",
                "Environment :: Console",
                "Intended Audience :: Science/Research",
                "License :: OSI Approved :: MIT License",
-               "Operating System :: OS Independent",
+               "Operating System :: Linux",
                "Programming Language :: Python",
                "Topic :: Scientific/Engineering"]
 
 # Description should be a one-liner:
-description = "aging: a template for small scientific Python projects"
+description = "Repo site for AGING prediction using neuroimaging descriptors. "
 # Long description will go up on the pypi page
 long_description = """
 
-Shablona
+Aging
 ========
-Shablona is a template project for small scientific Python projects.
-
-It contains software implementations of an analysis of some simple data, but
-more importantly, it contains infrastructure for testing, documentation,
-continuous integration and deployment, which can be easily adapted
-to use in other projects.
-
 To get started using these components in your own software, please go to the
 repository README_.
 
@@ -52,21 +45,20 @@ for usage, and a DISCLAIMER OF ALL WARRANTIES.
 
 All trademarks referenced herein are property of their respective holders.
 
-Copyright (c) 2015--, Ariel Rokem, The University of Washington
-eScience Institute.
+Copyright (c) 2017--, Asier Erramuzpe, BioCruces Health Research Institute.
 """
 
 NAME = "aging"
-MAINTAINER = "Ariel Rokem"
-MAINTAINER_EMAIL = "arokem@gmail.com"
+MAINTAINER = "Asier Erramuzpe"
+MAINTAINER_EMAIL = "asier.erramuzpe@gmail.com"
 DESCRIPTION = description
 LONG_DESCRIPTION = long_description
 URL = "http://github.com/erramuzpe/aging"
 DOWNLOAD_URL = ""
 LICENSE = "MIT"
-AUTHOR = "Ariel Rokem"
-AUTHOR_EMAIL = "arokem@gmail.com"
-PLATFORMS = "OS Independent"
+AUTHOR = "Asier Erranuzpe"
+AUTHOR_EMAIL = "asier.erramuzpe@gmail.com"
+PLATFORMS = "Linux"
 MAJOR = _version_major
 MINOR = _version_minor
 MICRO = _version_micro
