21966c1c8f31848351f91b202ba18d5ff1d36728
erramuzpe <asier.erramuzpe@gmail.com>
2017-02-02 13:43:22 +0100

    added preproc.py

---
+++ preproc.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+# Module for data preparation
+
+# Generate SCPIL AND FCPIL goes here
+
+# Read data
+
+# Organize
+
+# Create data structures
+
+# From here to Mod Creation -> Analyze and rethink problem
