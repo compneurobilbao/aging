a360ba978d8a931160d6dc8a6a83c7438516ae41
erramuzpe <asier.erramuzpe@gmail.com>
2017-02-07 09:19:05 +0100

    tree implementation, getting there

--- scripts/tree_experiments.py
+++ scripts/tree_experiments.py
@@ -12,103 +12,74 @@ import scipy.io as sio
 from scipy.spatial.distance import pdist
 from scipy.cluster import hierarchy
 import numpy as np
-# import aging.proc as pr
+import pickle
+
+import aging as ag
+
+data_path = op.join(ag.__path__[0], 'data')
+hc_data_path = op.join(data_path, 'hc')
 
 
-def getNewick(node, newick, parentdist, leaf_names):
+def get_newick(node, newick, parentdist, leaf_names):
     if node.is_leaf():
-        return "%s:%.2f%s" % (leaf_names[node.id], parentdist - node.dist, newick)
+        return '%s:%f%s' % (leaf_names[node.id],
+                            parentdist - node.dist, newick)
     else:
         if len(newick) > 0:
-            newick = "):%.2f%s" % (parentdist - node.dist, newick)
+            newick = '):%f%s' % (parentdist - node.dist, newick)
         else:
-            newick = ");"
-        newick = getNewick(node.get_left(), newick, node.dist, leaf_names)
-        newick = getNewick(node.get_right(), ",%s" % (newick), node.dist, leaf_names)
-        newick = "(%s" % (newick)
+            newick = ');'
+        newick = get_newick(node.get_left(), newick,
+                            node.dist, leaf_names)
+        newick = get_newick(node.get_right(), ',%s' % (newick),
+                            node.dist, leaf_names)
+        newick = '(%s' % (newick)
         return newick
 
 
-hc_data_path = "/home/asier/Desktop/hc_tools"
-partition = op.join(hc_data_path, 'average_networks_12.mat')
+def create_hc_tree():
+    from ete3 import Tree
 
-data = sio.loadmat(partition)
-func_network = data['func_network']
+    partition = op.join(hc_data_path, 'average_networks_12.mat')
 
-Y = pdist(func_network, 'cosine')
-Z = hierarchy.linkage(Y,  method='weighted')
+    data = sio.loadmat(partition)
+    func_network = data['func_network']
 
-leaf_names = [str(i) for i in np.arange(1, 2515)]
+    Y = pdist(func_network, 'cosine')
+    Z = hierarchy.linkage(Y,  method='weighted')
 
-tree = hierarchy.to_tree(Z, False)
-newick = getNewick(tree, "", tree.dist, leaf_names)
+    leaf_names = [str(i) for i in np.arange(1, 2515)]
 
+    tree = hierarchy.to_tree(Z, False)
+    newick = get_newick(tree, '', tree.dist, leaf_names)
 
-from ete3 import Tree
-
-t = Tree(newick)
-print(t)
-t.show()
-
-search_by_size(t, 50)
-len(t)
-len(t.children)
-
-t.describe()
-    
-t.children[0].describe()
-
-t.get_descendants()[0].describe()
-t.get_leaf_names()
+    hc_tree = Tree(newick)
+    pickle.dump(hc_tree, open(op.join(hc_data_path, 'hc_tree.p'), 'wb'))
 
 
 def root_dist(node):
     return node.get_distance(node.get_tree_root())
 
 
-def nodes_from_level(nodelist, partition_end):
-    #print(len(nodelist))
-    if len(nodelist) == partition_end:
-        return nodelist
+def nodes_from_level(node_list, partition_end):
+    if len(node_list) == partition_end:
+        return node_list
     else:
-        distances = [root_dist(node) for node in nodelist]
+        distances = [root_dist(node) for node in node_list]
         min_dist_idx = np.argmin(distances)
 
-        nodelist.append(nodelist[min_dist_idx].children[0])
-        nodelist.append(nodelist[min_dist_idx].children[1])
-        nodelist.remove(nodelist[min_dist_idx])
+        node_list.append(node_list[min_dist_idx].children[0])
+        node_list.append(node_list[min_dist_idx].children[1])
+        node_list.remove(node_list[min_dist_idx])
 
-        return nodes_from_level(nodelist, partition_end)
-
-
-
-for i in range(1,20):
-    a = nodes_from_level([t], i)
-    b = [t.get_leaf_names() for t in a]
-    [print(len(c)) for c in b]  
-    print("")
-     
+        return nodes_from_level(node_list, partition_end)
 
 
+def get_leaf_from_list(node_list):
+    return [node.get_leaf_names() for node in node_list]
 
-for node in t.iter_descendants("levelorder"):
-    print(node.name, node.get_distance(node.get_tree_root()))
 
-for node in t.iter_descendants("levelorder"):
-    print(node.name, node.dist)
-  
-    
-    
-    
-def search_by_size(node, size):
-    "Finds nodes with a given number of leaves"
-    matches = []
-    for n in node.traverse():
-       if len(n) == size:
-          matches.append(n)
-    return matches
+def get_partition_leaf(partition_level):
+    pass
 
-    
-t = Tree('((D,F)E,(B,H)B);', format=8)    
-print(t)
-    
+
