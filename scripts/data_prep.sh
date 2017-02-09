2ee82ba7e6a0763bf340bd1e18698a578bb163a8
erramuzpe <asier.erramuzpe@gmail.com>
2017-02-09 10:30:30 +0100

    batch file

---
+++ scripts/data_prep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#SBATCH -p large # Partition to submit to
+#SBATCH -n 40 # Number of cores
+#SBATCH --mem=50000 # Memory pool for all cores
+#SBATCH -o out # File to which STDOUT will be written
+#SBATCH -e err # File to which STDERR will be written
+
+python data_prep.py
