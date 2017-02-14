5155d61c3cc733d42807f0d754a43199d7b7a573
erramuzpe <asier.erramuzpe@gmail.com>
2017-02-14 10:18:12 +0100

    internal connectivity calculation

--- aging/proc.py
+++ aging/proc.py
@@ -4,6 +4,7 @@ from itertools import product
 import os
 import aging as ag
 import numpy as np
+from scipy import stats, linalg
 
 data_path = os.path.join(ag.__path__[0], 'data')
 aging_data_dir = os.path.join(data_path, 'subjects')
@@ -19,5 +20,96 @@ due.cite(Doi(""),
          description="",
          tags=[""],
          path='aging')
+
+MAX_PART = 2514
+
+
+
+
+def p_corr(C):
+    """
+    Partial Correlation in Python (clone of Matlab's partialcorr)
+    This uses the linear regression approach to compute the partial 
+    correlation (might be slow for a huge number of variables). The 
+    algorithm is detailed here:
+        http://en.wikipedia.org/wiki/Partial_correlation#Using_linear_regression
+    Taking X and Y two variables of interest and Z the matrix with all the variable minus {X, Y},
+    the algorithm can be summarized as
+        1) perform a normal linear least-squares regression with X as the target and Z as the predictor
+        2) calculate the residuals in Step #1
+        3) perform a normal linear least-squares regression with Y as the target and Z as the predictor
+        4) calculate the residuals in Step #3
+        5) calculate the correlation coefficient between the residuals from Steps #2 and #4; 
+        The result is the partial correlation between X and Y while controlling for the effect of Z
+    Date: Nov 2014
+    Author: Fabian Pedregosa-Izquierdo, f@bianp.net
+    Testing: Valentina Borghesani, valentinaborghesani@gmail.com
+    """
+    """
+    Returns the sample linear partial correlation coefficients between pairs of variables in C, controlling 
+    for the remaining variables in C.
+    Parameters
+    ----------
+    C : array-like, shape (n, p)
+        Array with the different variables. Each column of C is taken as a variable
+    Returns
+    -------
+    P : array-like, shape (p, p)
+        P[i, j] contains the partial correlation of C[:, i] and C[:, j] controlling
+        for the remaining variables in C.
+    """
+    
+    C = np.asarray(C)
+    p = C.shape[1]
+    P_corr = np.zeros((p, p), dtype=np.float)
+    for i in range(p):
+        P_corr[i, i] = 1
+        for j in range(i+1, p):
+            idx = np.ones(p, dtype=np.bool)
+            idx[i] = False
+            idx[j] = False
+            beta_i = linalg.lstsq(C[:, idx], C[:, j])[0]
+            beta_j = linalg.lstsq(C[:, idx], C[:, i])[0]
+
+            res_j = C[:, j] - C[:, idx].dot( beta_i)
+            res_i = C[:, i] - C[:, idx].dot(beta_j)
+            
+            corr = stats.pearsonr(res_i, res_j)[0]
+            P_corr[i, j] = corr
+            P_corr[j, i] = corr
+        
+    return P_corr
+
+
+def internal():
+    # TODO: Significant pvlues for partial_corr
+    internal_fc_cn = ['' for j in range(MAX_PART)]
+    internal_sc_cn = ['' for j in range(MAX_PART)]
+
+    age = np.load(os.path.join(container_data_dir, 'age.npy'))
+    dti_motion = np.load(os.path.join(container_data_dir, 'dti_motion.npy'))
+    fmri_motion = np.load(os.path.join(container_data_dir, 'fmri_motion.npy'))
+
+    for nMod in range(2, 999):
+        print(nMod)
+        data = np.load(os.path.join(mod_data_dir, 'mod_{}.npz'.format(nMod)))
+        FC_Mod = data.f.FC_Mod
+        SC_Mod = data.f.SC_Mod
+
+        internal_fc_cn[nMod] = np.zeros(nMod)
+        internal_sc_cn[nMod] = np.zeros(nMod)
+
+        for i in range(nMod):
+            # (out-degree)
+            internal_fc_cn[nMod][i] = p_corr(np.column_stack((FC_Mod[i, i, :],
+                                                              age,
+                                                              fmri_motion)))[0, 1]
+
+            internal_sc_cn[nMod][i] = p_corr(np.column_stack((SC_Mod[i, i, :],
+                                                              age,
+                                                              dti_motion)))[0, 1]
+
+    np.save(os.path.join(container_data_dir, 'internal_fc_cn'), internal_fc_cn)
+    np.save(os.path.join(container_data_dir, 'internal_sc_cn'), internal_fc_cn)
 
 
