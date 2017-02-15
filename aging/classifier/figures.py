df7edbe74aead20c506fcd53da784d17acc88f83
erramuzpe <asier.erramuzpe@gmail.com>
2017-02-15 10:18:07 +0100

    added figures script

---
+++ aging/classifier/figures.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 10:28:42 2016
+
+@author: asier
+"""
+
+## FIGURES
+
+import numpy  as np
+import seaborn as sns
+import pandas as pd
+
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
+
+import matplotlib.pyplot as plt
+
+###############
+# PANEL 1 - MLE corr and MLE corr shuf
+
+
+MLE = np.squeeze(np.load('corVsDesc_MLE.npy'))
+MLE_shuf = np.squeeze(np.load('corVsDesc_MLE_shuf.npy'))
+sigma_shuf = np.squeeze(np.load('sigma_MLE_shuf.npy'))
+
+x = np.linspace(0, 200, 200)
+plt.plot(x, MLE, label='MLE')
+
+plt.hold(True)
+
+plt.plot(x, MLE_shuf, color='#CC4F1B', label='MLE shuffled')
+plt.fill_between(x, MLE_shuf-sigma_shuf, MLE_shuf+sigma_shuf, alpha=0.5, 
+                 edgecolor='#CC4F1B', facecolor='#FF9848') 
+
+plt.axis([0, 200, 0, 1])
+plt.xlabel('Number of Descriptors')
+plt.ylabel('corr(MLE, age)')
+plt.legend()
+plt.savefig('/home/asier/Desktop/AGING/motion_correction/figures/fig5/panel1.eps', format='eps', dpi=1000)
+
+
+
+
+###############
+# PANEL 4- MAE and MAE shuf
+
+ordered_data = np.load('ordered_data_ext_int_mae.npy')
+results = np.squeeze(np.load('results_ext_int_mae.npy'))
+sigma = np.squeeze(np.load('sigma_ext_int_mae.npy'))
+
+
+
+results_shuf = np.squeeze(np.load('results_ext_int_shuf_mae.npy'))
+sigma_shuf = np.squeeze(np.load('sigma_ext_int_shuf_mae.npy'))
+
+
+x = np.linspace(0, 200, 200)
+plt.plot(x, results, label='MAE')
+plt.fill_between(x, results-sigma, results+sigma, alpha=0.5) 
+
+plt.hold(True)
+
+plt.plot(x, results_shuf, color='#CC4F1B', label='MAE shuffled')
+plt.fill_between(x, results_shuf-sigma_shuf, results_shuf+sigma_shuf, alpha=0.5, 
+                 edgecolor='#CC4F1B', facecolor='#FF9848') 
+
+
+plt.axis([0, 100, 0, 30])
+plt.xlabel('Number of Descriptors')
+plt.ylabel('Cross-Validated MAE')
+plt.legend()
+plt.savefig('/home/asier/Desktop/AGING/motion_correction/figures/fig5/panel4.eps', format='eps', dpi=1000)
+
+
+###############
+# PANEL 5- prediction MAE ## ASK WHAT TO DO
+
+print(np.argmin(results[1:114])) # 25 descriptors
+print(np.min(results[1:114])) # 6.96142488891
+
+
+
+number_of_descriptors = np.argmin(results[1:114])
+y = pd.read_csv('age.csv', header=None)
+X = ordered_data[:,:number_of_descriptors]
+
+for i in range(100):
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    
+    lm = LinearRegression()
+    lm.fit(X_train, y_train)
+    y_pred = lm.predict(X_test)
+    m = metrics.mean_absolute_error(y_test, y_pred)
+    print(m)
+    if (m < np.min(results[1:])+0.1) and (m > np.min(results[1:])-0.1): break
+
+import scipy
+print(scipy.stats.pearsonr(y_test, y_pred))
+
+plt.scatter(y_pred,y_test)
+plt.plot(range(100),range(100))
+plt.xlabel("predicted age")
+plt.ylabel("real age")
+plt.savefig('/home/asier/Desktop/AGING/motion_correction/figures/fig5/panel5.eps', format='eps', dpi=1000)
+ 
