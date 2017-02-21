ebd350ce22be16d209a7e4f933c9dfd6246a3567
erramuzpe <asier.erramuzpe@gmail.com>
2017-02-21 11:42:27 +0100

    prediction error age dependant

--- aging/classifier/prediction_error_with_variables.py
+++ aging/classifier/prediction_error_with_variables.py
@@ -110,3 +110,74 @@ plt.scatter(beck[idx_limited_beck], error[idx_155_beck])
 plt.ylabel("prediction error")
 plt.xlabel("beck")
 print(scipy.stats.pearsonr(beck[idx_limited_beck], error[idx_155_beck]))                    
+
+
+
+print(scipy.stats.pearsonr(ipaqq[idx_limited_ipaqq], y[idx_155_ipaqq,0] ))
+print(scipy.stats.pearsonr(motor[idx_limited_motor], y[idx_155_motor,0] ))
+print(scipy.stats.pearsonr(beck[idx_limited_beck], y[idx_155_beck,0] ))
+
+
+# MoCa -MCI
+moca = np.array(info_limited.MoCa)
+moca = np.array([subj_id for i, subj_id in enumerate(moca) if not np.isnan(info_limited.MoCa[i])])
+ID_moca = [str(subj_id) for i, subj_id in enumerate(info_ID) if not np.isnan(info_limited.MoCa[i])]
+
+idx_155_moca = [i for i, subj in enumerate(id_155) if subj in ID_moca]           
+idx_limited_moca = [i for i, subj in enumerate(ID_moca) if subj in id_155]
+
+plt.scatter(moca[idx_limited_moca], error[idx_155_moca])
+plt.ylabel("prediction error")
+plt.xlabel("moca")
+print(scipy.stats.pearsonr(moca[idx_limited_moca], error[idx_155_moca]))                    
+
+
+
+print(scipy.stats.pearsonr(ipaqq[idx_limited_ipaqq], y[idx_155_ipaqq,0] ))
+print(scipy.stats.pearsonr(motor[idx_limited_motor], y[idx_155_motor,0] ))
+print(scipy.stats.pearsonr(beck[idx_limited_beck], y[idx_155_beck,0] ))
+
+
+
+plt.scatter(info_limited.MoCa, info_limited.Age)
+
+
+
+
+
+ipaqq = np.array(info_limited.IpaqTotal)
+ipaqq = np.array([subj_id for i, subj_id in enumerate(ipaqq) if not np.isnan(info_limited.IpaqTotal[i]) and info_limited.Age[i] > 25])
+ID_ipaqq = [str(subj_id) for i, subj_id in enumerate(info_ID) if not np.isnan(info_limited.IpaqTotal[i]) and info_limited.Age[i] > 25]
+
+idx_155_ipaqq = [i for i, subj in enumerate(id_155) if subj in ID_ipaqq]           
+idx_limited_ipaqq = [i for i, subj in enumerate(ID_ipaqq) if subj in id_155]           
+
+
+plt.scatter(ipaqq[idx_limited_ipaqq], error[idx_155_ipaqq])
+plt.ylabel("prediction error")
+plt.xlabel("ipaqq")
+print(scipy.stats.pearsonr(ipaqq[idx_limited_ipaqq], error[idx_155_ipaqq]))                    
+
+
+
+
+motor = np.array(info_limited.MeanBoth)
+motor = np.array([subj_id for i, subj_id in enumerate(motor) if not np.isnan(info_limited.MeanBoth[i]) and info_limited.Age[i] > 30])
+ID_motor = [str(subj_id) for i, subj_id in enumerate(info_ID) if not np.isnan(info_limited.MeanBoth[i]) and info_limited.Age[i] > 30]
+
+idx_155_motor = [i for i, subj in enumerate(id_155) if subj in ID_motor]           
+idx_limited_motor = [i for i, subj in enumerate(ID_motor) if subj in id_155] 
+
+plt.scatter(motor[idx_limited_motor], error[idx_155_motor])
+plt.ylabel("prediction error")
+plt.xlabel("motor")
+print(scipy.stats.pearsonr(motor[idx_limited_motor], error[idx_155_motor]))                    
+
+
+
+### pcorr
+from scipy import stats # and import p_corr from aging.proc
+p_corr(error[idx_155_ipaqq] , motor[idx_limited_ipaqq], y[idx_155_ipaqq,0])
+p_corr(error[idx_155_motor] , motor[idx_limited_motor], y[idx_155_motor,0])
+p_corr(error[idx_155_moca] , motor[idx_limited_moca], y[idx_155_moca,0])
+p_corr(error[idx_155_beck] , motor[idx_limited_beck], y[idx_155_beck,0])
