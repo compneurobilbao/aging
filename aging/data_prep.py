0152dc91a04602ad9b3fc92438823fdb72979a0a
erramuzpe <asier.erramuzpe@gmail.com>
2017-05-11 12:49:46 +0200

    data_prep.py cleaned, missing all docstrings and tests :/

--- aging/data_prep.py
+++ aging/data_prep.py
@@ -9,18 +9,18 @@ import numpy as np
 
 DATA_PATH = opj(ag.__path__[0], 'data')
 AGING_DATA_DIR = opj(DATA_PATH, 'subjects')
-CONTAINER_DATA_DIR = opj(DATA_PATH, 'container_data')  # ID_subj,FCpil
+CONTAINER_DATA_DIR = opj(DATA_PATH, 'container_data')  # id_subj,FCpil
 MOD_DATA_DIR = opj(DATA_PATH, 'mods')
 
 
 # Create SC and FC matrices
-def generate_age_sex_ID():
+def generate_age_sex_id():
     age_subj = []
     sex_subj = []
-    ID_subj = os.listdir(AGING_DATA_DIR)
-    ID_subj.sort()
+    id_subj = os.listdir(AGING_DATA_DIR)
+    id_subj.sort()
 
-    for idx in ID_subj:
+    for idx in id_subj:
         folder_path = opj(AGING_DATA_DIR, idx)
         age = np.loadtxt(opj(folder_path, 'age.txt'))
         sex = np.loadtxt(opj(folder_path, 'gender.txt'), dtype='str')
@@ -29,15 +29,15 @@ def generate_age_sex_ID():
 
     np.save(opj(CONTAINER_DATA_DIR, 'age'), age_subj)
     np.save(opj(CONTAINER_DATA_DIR, 'sex'), sex_subj)
-    np.save(opj(CONTAINER_DATA_DIR, 'ID_subj'), ID_subj)
+    np.save(opj(CONTAINER_DATA_DIR, 'id_subj'), id_subj)
     return
 
 
-def generate_FC(ID_subj):
+def generate_FC(id_subj):
 
-    FC_matrix = np.empty((2514, 2514, len(ID_subj)), dtype='float32')
+    FC_matrix = np.empty((2514, 2514, len(id_subj)), dtype='float32')
 
-    for i, idx in enumerate(ID_subj):
+    for i, idx in enumerate(id_subj):
         folder_path = opj(AGING_DATA_DIR, idx)
         time_series = np.load(opj(folder_path, 'time_series.npy'))
         fc = np.corrcoef(time_series)
@@ -49,11 +49,11 @@ def generate_FC(ID_subj):
     return FC_matrix
 
 
-def generate_SC(ID_subj):
+def generate_SC(id_subj):
 
-    SC_matrix = np.empty((2514, 2514, len(ID_subj)), dtype='int16')
+    SC_matrix = np.empty((2514, 2514, len(id_subj)), dtype='int16')
 
-    for i, idx in enumerate(ID_subj):
+    for i, idx in enumerate(id_subj):
         folder_path = opj(AGING_DATA_DIR, idx)
         fiber_num = np.load(opj(folder_path, 'fiber_num.npy'))
         # TODO: Try Sparse - not good for concatenation
@@ -68,35 +68,35 @@ def generate_data_containers():
     if not os.path.exists(CONTAINER_DATA_DIR):
         os.makedirs(CONTAINER_DATA_DIR)
 
-    generate_age_sex_ID()
+    generate_age_sex_id()
 
 
 def remove_corrupted_rois(FC_matrix, SC_matrix):
-    ROI_part = np.loadtxt(opj(CONTAINER_DATA_DIR,
+    roi_part = np.loadtxt(opj(CONTAINER_DATA_DIR,
                               'rois_full_inside_ventricles.txt'),
                           dtype='int')
-    ROI_in = np.loadtxt(opj(CONTAINER_DATA_DIR,
+    roi_in = np.loadtxt(opj(CONTAINER_DATA_DIR,
                             'rois_part_inside_ventricles.txt'),
                         dtype='int')
 
-    # -1 due to python indexing and ROI = 0 not existing
-    ROI_2ex = np.concatenate((ROI_part, ROI_in), axis=0) - 1
+    # -1 due to python indexing and roi = 0 not existing
+    roi_2ex = np.concatenate((roi_part, roi_in), axis=0) - 1
 
-    FC_matrix[ROI_2ex, :, :] = 0
-    FC_matrix[:, ROI_2ex, :] = 0
-    SC_matrix[ROI_2ex, :, :] = 0
-    SC_matrix[:, ROI_2ex, :] = 0
+    FC_matrix[roi_2ex, :, :] = 0
+    FC_matrix[:, roi_2ex, :] = 0
+    SC_matrix[roi_2ex, :, :] = 0
+    SC_matrix[:, roi_2ex, :] = 0
 
     return FC_matrix, SC_matrix
 
 
-def generate_mod(nMod, FC_matrix, SC_matrix, modules_info, ID_subj):
+def generate_mod(nMod, FC_matrix, SC_matrix, modules_info, id_subj):
     from numpy import squeeze as sq
 
     modules_idx = modules_info[nMod-1, :nMod]-1  # -1 due to 0 indexing
 
-    FC_Mod = np.empty((nMod, nMod, len(ID_subj)), dtype='float32')
-    SC_Mod = np.empty((nMod, nMod, len(ID_subj)), dtype='float32')
+    FC_Mod = np.empty((nMod, nMod, len(id_subj)), dtype='float32')
+    SC_Mod = np.empty((nMod, nMod, len(id_subj)), dtype='float32')
 
     for i, j in product(range(nMod), range(nMod)):
         if modules_idx[i].shape[0] == 1 or modules_idx[j].shape[0] == 1:
@@ -125,16 +125,16 @@ def build_FC_SC_mods():
     if not os.path.exists(MOD_DATA_DIR):
         os.makedirs(MOD_DATA_DIR)
 
-    ID_subj = np.load(opj(CONTAINER_DATA_DIR, 'ID_subj.npy'))
+    id_subj = np.load(opj(CONTAINER_DATA_DIR, 'id_subj.npy'))
 
-    FC_SC_matrix = opj(CONTAINER_DATA_DIR, 'FC_SC_matrix.npz')
-    if not os.path.exists(FC_SC_matrix):
-        FC_matrix = generate_FC(ID_subj)
-        SC_matrix = generate_SC(ID_subj)
+    FC_SC_matrix_path = opj(CONTAINER_DATA_DIR, 'FC_SC_matrix.npz')
+    if not os.path.exists(FC_SC_matrix_path):
+        FC_matrix = generate_FC(id_subj)
+        SC_matrix = generate_SC(id_subj)
         FC_matrix, SC_matrix = remove_corrupted_rois(FC_matrix, SC_matrix)
-        np.savez(FC_SC_matrix, FC_matrix=FC_matrix, SC_matrix=SC_matrix)
+        np.savez(FC_SC_matrix_path, FC_matrix=FC_matrix, SC_matrix=SC_matrix)
     else:
-        FC_SC_matrix = np.load(FC_SC_matrix)
+        FC_SC_matrix = np.load(FC_SC_matrix_path)
         FC_matrix = FC_SC_matrix.f.FC_matrix
         SC_matrix = FC_SC_matrix.f.SC_matrix
 
@@ -149,14 +149,14 @@ def build_FC_SC_mods():
                          FC_matrix,
                          SC_matrix,
                          modules_info,
-                         ID_subj)
+                         id_subj)
 
 
 if __name__ == "__main__":
     import sys
 
     if not os.path.exists(opj(CONTAINER_DATA_DIR,
-                                       'ID_subj.npy')):
+                              'id_subj.npy')):
         generate_data_containers()
     print('Data containers generated')
     build_FC_SC_mods()
